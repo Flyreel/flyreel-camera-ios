730327e1f6f86b526772b92d0e920b77f2e9e6b6
Szymon Wojcik <szymon.wojcik@lexisnexisrisk.com>
2024-05-21 14:01:15 +0200

    An initial binary version

---
+++ FlyreelCamera.xcframework/ios-arm64_x86_64-simulator/FlyreelCamera.framework/Headers/flyreel_camera.h
@@ -0,0 +1,18 @@
+//
+//  flyreel_camera.h
+//  flyreel-camera
+//
+//  Created by Wojcik, Szymon on 14/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for flyreel_camera.
+FOUNDATION_EXPORT double flyreel_cameraVersionNumber;
+
+//! Project version string for flyreel_camera.
+FOUNDATION_EXPORT const unsigned char flyreel_cameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <flyreel_camera/PublicHeader.h>
+
+
